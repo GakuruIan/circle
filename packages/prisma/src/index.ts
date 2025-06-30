1189a1257f2ac8c2a37633c4b7344c50ca40c6d5
Gakuru <iangakuru146@gmail.com>
2025-07-01 01:53:51 +0300

    Add Google Services files to .gitignore to prevent API key exposure

--- packages/prisma/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./generated/client";
-
-export { PrismaClient } from "./generated/client";
-
-export type { User, Prisma } from "./generated/client";
-
-// Export custom types or utilities goes here:
